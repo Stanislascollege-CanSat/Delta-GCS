4f1277b4b1d502a0c1d0f51fd6cee9ce4529e2f9
Jesse Rodrigo <39565615+JSSRDRG@users.noreply.github.com>
2018-11-22 01:27:36 +0100

    Removed some comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
+// Delta-MCU microcontroller software.
 #include <Arduino.h>
 
 int x = 0;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   Serial.print("{GPS:");
   Serial.print(x);
   Serial.print(";AcX:12,33;RSI:-68;}");
